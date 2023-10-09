0502ed8fefc6125756908bc0a11669b611fa1790
Tim Hawes <trhawes@gmail.com>
2023-10-08 22:42:39 -0400

    Added new character, Siobhán MacAuley. And her books.

--- public/js/main.js
+++ public/js/main.js
@@ -12,11 +12,30 @@ function closeNav() {
   //document.getElementById("side-nav").style.width = "0%";
 }
 
+/* Returns the root directory of a page. */
 function getRootDir() {
   let pathSegments = window.location.pathname.split('/');
   if (pathSegments.length > 2) {
     return pathSegments[1];
   } else {
     return '';
+  }
+}
+
+
+function collapsableContainers() {
+  let coll = document.getElementsByClassName("collapsible");
+  let i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
   }
 }
