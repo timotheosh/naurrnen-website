7873dc98aa03b0e921a1606cac41c465f58b7a4c
Tim Hawes <trhawes@gmail.com>
2023-10-09 21:56:58 -0400

    Move all Javascript into main.js. Implement addEventListener. Add Nations page.

--- public/js/main.js
+++ public/js/main.js
@@ -39,3 +39,30 @@ function collapsableContainers() {
     });
   }
 }
+
+function expandRootMenu() {
+  // My dogfood
+  let path = getRootDir();
+  if (path.length > 0) {
+    let element = document.getElementById(path);
+    element.classList.toggle("active");
+    element.nextElementSibling.style.display = "block";
+  }
+}
+
+function toggleContainer() {
+  // Code copy-pasted from w3schools
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+}
