59f9b7c2879787586e322535c13ddac20e8aa26f
Tim Hawes <trhawes@gmail.com>
2023-11-02 05:18:16 -0400

    Add images for nations.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,11 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("side-nav").style.width = "480px";
-  //document.getElementById("main").style.marginLeft = "480px";
-  //document.getElementById("side-nav").style.width = "100%";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("side-nav").style.width = "0";
-  //document.getElementById("main").style.marginLeft = "0";
-  //document.getElementById("side-nav").style.width = "0%";
 }
 
 /* Returns the root directory of a page. */
