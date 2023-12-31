f3a400f1c2c6289a0b3389ce7b7026d0c3467f95
Tim Hawes <trhawes@gmail.com>
2023-12-31 08:21:36 -0500

    Correct the format of .dir-locals-el file.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-(("gimel-auto-publish" . ((gimel-auto-publish . t))))
+((nil . ((gimel-auto-publish . t))))
