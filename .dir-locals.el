ef9dc43499020447646d3d18d79663ebb83f7c10
Tim Hawes <trhawes@gmail.com>
2024-12-07 22:46:48 -0500

    Added new character, Calahir Vaelora

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((gimel-auto-publish . t))))
+((nil . ((gimel-auto-publish . t)
+         (gimel-api-endpoint . "http://localhost:8088"))))
